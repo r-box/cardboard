360ad242f36079e1716fb9d76e827f72db0dcad0
Ian Lyttle <ijlyttle@users.noreply.github.com>
2022-01-20 13:58:20 -0600

    update docuementation

--- R/auth-use.R
+++ R/auth-use.R
@@ -1,5 +1,12 @@
 #' Default auth function
 #'
+#' @description
+#' This package lets you set a *default* auth function.
+#' Use these functions to accesss this function:
+#'
+#'  - `bx_auth_use()`: to set the default function
+#'  - `bx_auth()`: to get the default function
+#'
 #' @inherit bx_auth_get params return
 #'
 #' @export
