b98b2bdf4045642cfd976cd5113adbc49da8d1ef
Ian Lyttle <ijlyttle@users.noreply.github.com>
2022-01-19 22:12:21 -0600

    do less

--- R/auth-encrypt-decrypt.R
+++ R/auth-encrypt-decrypt.R
@@ -22,17 +22,14 @@ as_key <- function(key) {
 
 # TODO: credit gargle
 secret_gen <- function() {
-  withr::local_seed(ms_since_midight())
+
+  # uncouple from seed
+  withr::local_preserve_seed()
+  set.seed(NULL)
 
   x <- sample(c(letters, LETTERS, 0:9), 50, replace = TRUE)
   paste0(x, collapse = "")
 }
-
-ms_since_midight <- function() {
-  dt <- Sys.time() - as.POSIXct(Sys.Date())
-  as.integer(unclass(dt) * 86400 * 1000)
-}
-
 
 as_nonce <- function(nonce) {
   nonce <- nonce %||% "d642b7509df52965ccfe8dcf99f94b91301b78579d9ceab2"
