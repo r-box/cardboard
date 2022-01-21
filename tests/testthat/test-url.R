2d7d7d3377d0e9c2375a6dd1a8910365ee91512a
Ian Lyttle <ijlyttle@users.noreply.github.com>
2022-01-20 19:25:05 -0600

    test auth

--- tests/testthat/test-url.R
+++ tests/testthat/test-url.R
@@ -9,3 +9,7 @@ test_that("url_box_api() works", {
   expect_identical(url_box_api("/foo"), "https://api.box.com/2.0/foo")
   expect_identical(url_box_api("/foo/bar"), "https://api.box.com/2.0/foo/bar")
 })
+
+test_that("auth works", {
+  expect_true(bx_auth_test(bx_auth()))
+})
